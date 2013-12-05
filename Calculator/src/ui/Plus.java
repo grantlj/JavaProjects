1d43b4be572ea0760b84834cdbed84d0ac419f6b
Liu Jiang <393444163@qq.com>
2013-12-05 17:44:22 +0800

    Delete Plus.java

--- Calculator/src/ui/Plus.java
+++
@@ -1,9 +0,0 @@
-package ui;
-
-class Plus extends BinaryOp {
-  Plus(Expr x, Expr y) { left = x; right = y; }
-  public String toString() { 
-    // we are the context
-    return super.toString(this, " + "); }
-  public boolean isSame(Expr e) { return e instanceof Plus; }
-}
